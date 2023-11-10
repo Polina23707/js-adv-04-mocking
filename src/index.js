810efc318597c13ec061032dd98151acb1062088
Polina Gorelik <Polina23707@gmail.com>
2023-11-10 22:24:06 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import './css/style.css';
+
+import './js/app';
+
+import fetchData from './js/http';
+
+export function getLevel(userId) {
+  const response = fetchData(`https://server/user/${userId}`);
+  
+  // TODO: логика обработки
+  if (response.status === 'ok') {
+     return `Ваш текущий уровень: ${response.level}`; 
+  }
+  
+  return `Информация об уровне временно недоступна`;
+}
+// TODO: write your code in app.js
+
+getLevel(1);g
